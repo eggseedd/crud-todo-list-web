77703d182ef713ad2ae04be1fd077c0216bfb86d
Raditya Yusuf <radityayusuf002@gmail.com>
2024-10-25 07:05:05 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,9 +17,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if ($this->app->environment('local')) {
             URL::forceScheme('https');
         }
     }
